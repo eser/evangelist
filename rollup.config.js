fda1bd5b6656e23a6cae89476936187c1840d643
Eser Ozvataf <eser@ozvataf.com>
2019-03-25 04:29:01 +0300

    Updated codebase.

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import typescript from 'rollup-plugin-typescript';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+    input: './src/index.ts',
+    output: {
+        file: './dist/index.min.js',
+        format: 'umd',
+        name: 'ponyfills',
+        compact: true,
+        sourcemap: true,
+    },
+    plugins: [
+        typescript({
+            tsconfig: './',
+        }),
+        nodeResolve({
+            extensions: [ '.mjs', '.js', '.ts', '.json' ],
+        }),
+        commonjs({
+            extensions: [ '.mjs', '.js', '.ts', '.json' ],
+        }),
+    ],
+};
